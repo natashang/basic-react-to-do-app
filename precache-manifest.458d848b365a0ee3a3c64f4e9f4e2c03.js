7185c17b6c9667ad90c3d0ca0c6c49131ebfdb92
natashang <50751197+natashang@users.noreply.github.com>
2020-03-03 11:46:22 -0800

    Updates

---
+++ precache-manifest.458d848b365a0ee3a3c64f4e9f4e2c03.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08b0a9abd27ad72b6b0e0f41610168a6",
+    "url": "/basic-react-to-do-app/index.html"
+  },
+  {
+    "revision": "393729051ad5cb8a6322",
+    "url": "/basic-react-to-do-app/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "3d33393d9987293d4098",
+    "url": "/basic-react-to-do-app/static/css/main.f1eec35d.chunk.css"
+  },
+  {
+    "revision": "393729051ad5cb8a6322",
+    "url": "/basic-react-to-do-app/static/js/2.4b1d97c2.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/basic-react-to-do-app/static/js/2.4b1d97c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d33393d9987293d4098",
+    "url": "/basic-react-to-do-app/static/js/main.11a9f078.chunk.js"
+  },
+  {
+    "revision": "a96ec21d9aab44e3c723",
+    "url": "/basic-react-to-do-app/static/js/runtime-main.55701379.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/basic-to-do-app/precache-manifest.21fdd779fef849c4cdb79eda019a1319.js"
+  "/basic-react-to-do-app/precache-manifest.458d848b365a0ee3a3c64f4e9f4e2c03.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/basic-to-do-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/basic-react-to-do-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
